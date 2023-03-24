a696b8c2b6519e45409798eabd4a23a0b6e3faa8
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-25 00:28:13 +0100

    fix: modify

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,8 +2,8 @@ import PropTypes from 'prop-types';
 
 const About = ({ title }) => {
   return (
-    <section className="section about-section">
-      <h1 className="section-title">{title}</h1>
+    <section className='section about-section'>
+      <h1 className='section-title'>{title}</h1>
       <p>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni ex
         obcaecati, nisi suscipit laudantium at ratione quia pariatur illum sunt
