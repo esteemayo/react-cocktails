159c3837415ffcf165391775a4ab538844c00f9e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-25 00:25:13 +0100

    fix: modify

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { ToastContainer } from "react-toastify";
 import axios from "axios";
 
 import NavBar from "./components/NavBar";
+import { About, Home, SingleCockTail } from "pages";
 
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
