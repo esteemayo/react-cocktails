65c20154f648e3c6b1aec11ad2a977b1325f8aba
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-25 00:29:21 +0100

    fix: modify

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -2,8 +2,8 @@ import { Link } from 'react-router-dom';
 
 const Error = () => {
   return (
-    <section className="error-page section">
-      <div className="error-container">
+    <section className='error-page section'>
+      <div className='error-container'>
         <h1>Oops! It's a dead end</h1>
         <Link to="/" className="btn btn-primary">
           Home
