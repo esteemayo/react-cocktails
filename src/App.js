ee80d3de83b578781accb45af15c5dbb520cc1d3
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-25 00:26:10 +0100

    fix: modify

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { About, Home, SingleCockTail } from 'pages';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 
-axios.defaults.baseURL = "https://www.thecocktaildb.com/api/json/v1/1";
+axios.defaults.baseURL = 'https://www.thecocktaildb.com/api/json/v1/1';
 
 function App() {
   return (
