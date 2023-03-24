ec9531a1c4a4b643bc856448f6bc6c4005d5a339
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-25 00:42:46 +0100

    fix: modify

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -13,7 +13,7 @@ axios.interceptors.response.use(null, (error) => {
   if (!expectedError) {
     logger.log(error);
     toast(error.message);
-    // toast("An unexpected error occurred!");
+    // toast('An unexpected error occurred!');
   }
 
   return Promise.reject(error);
