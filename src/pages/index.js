9c332347204229004441b53c1eaefd4e5ca656ba
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-25 00:23:12 +0100

    feat: import all pages

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import About from './About';
+import Error from './Error';
+import Home from './Home';
+import SingleCockTail from './SingleCockTail';
+
+export { About, Error, Home, SingleCockTail };
