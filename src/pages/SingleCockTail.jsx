7ed52cc77be8bf1dc0750aa8dd09205c6b305ebe
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-25 00:32:51 +0100

    fix: modify

--- src/pages/SingleCockTail.jsx
+++ src/pages/SingleCockTail.jsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
 
 import Loading from "components/Loading";
 
