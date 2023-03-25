7f243cb46013517f9e0b963a5f411d3e8188106a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-25 01:04:34 +0100

    fix: modify

--- src/context/cocktail/CocktailContext.js
+++ src/context/cocktail/CocktailContext.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext, createContext } from "react";
+import { useState, useEffect, useContext, createContext } from 'react';
 
 import { getCocktails } from "../../services/cocktailService";
 
