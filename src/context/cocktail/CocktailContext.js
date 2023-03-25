82a458faba6186c34ae1bbb197a85749fdbbd04e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-25 01:03:11 +0100

    fix: change file name

---
+++ src/context/cocktail/CocktailContext.js
@@ -0,0 +1,48 @@
+import { useState, useEffect, useContext, createContext } from "react";
+
+import { getCocktails } from "../../services/cocktailService";
+
+const AppContext = createContext();
+
+const AppProvider = ({ children }) => {
+  const [searchTerm, setSearchTerm] = useState("a");
+  const [cocktails, setCocktails] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async function fetchCocktails() {
+      const { data } = await getCocktails(searchTerm);
+      const { drinks } = data;
+
+      if (drinks) {
+        const newCocktails = drinks.map((item) => {
+          const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } =
+            item;
+          return {
+            id: idDrink,
+            name: strDrink,
+            image: strDrinkThumb,
+            info: strAlcoholic,
+            glass: strGlass,
+          };
+        });
+        setCocktails(newCocktails);
+        setLoading(false);
+      } else {
+        setCocktails([]);
+      }
+    })();
+  }, [searchTerm]);
+
+  return (
+    <AppContext.Provider value={{ loading, cocktails, setSearchTerm }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
