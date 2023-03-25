836cb1703e51f1445112421c58e37e95a8ef60d8
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-25 01:11:21 +0100

    fix: refactor code

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { AppProvider } from './context/cocktail/CocktailContext';
+import AppProvider from './context/cocktail/CocktailContext';
 
 ReactDOM.render(
   <React.StrictMode>
