89e08362d78a48f7a8497bf36e58d4be314cd157
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-25 01:02:13 +0100

    fix: change file folder

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { AppProvider } from './context/context';
+import { AppProvider } from './context/cocktail/context';
 
 ReactDOM.render(
   <React.StrictMode>
