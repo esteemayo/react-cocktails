5090da619ade6757bd221aacfa6513366b98216c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-25 01:16:02 +0100

    fix: add index.css

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import AppProvider from 'context/cocktail/CocktailContext';
 
+import './index.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <AppProvider>
